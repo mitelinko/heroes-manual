429fc3eaa7f83f984ec9f768f6acc6a29bd210b1
Tucker LoCicero <t.g.locicero@gmail.com>
2018-02-14 12:19:03 -0500

    perp info and victim info placeholder pages added

---
+++ HeroesManual/HeroesManual/HeroesManual/PerpInfoPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HeroesManual
+{
+    public partial class PerpInfoPage : ContentPage
+    {
+        public PerpInfoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
