7c9cdf7dc910fd7a18227cf9b0108f1423d011bc
Tucker LoCicero <t.g.locicero@gmail.com>
2018-03-30 10:17:16 -0400

    work in progress for quiz

---
+++ iOS/HeroesManual/ContentPages/Quiz_Content.swift
@@ -0,0 +1,157 @@
+//
+//  Quiz_Content.swift
+//  HeroesManual
+//
+//  Created by Tucker on 3/28/18.
+//  Copyright © 2018 Team Simba. All rights reserved.
+//
+
+import Foundation
+
+struct Q1 {
+    static let question = "What age group has the highest rate of intimate partner violence?"
+    static let a = "Teen girls and young childbearing women (18-24)"
+    static let b = "Women 35-45"
+    static let c = "Seniors (65+)"
+    static let answer = "a"
+    static let details = "Approximately “one in three adolescents in the U.S. is a victim of physical, sexual, emotional, or verbal abuse from a dating partner, a figure that far exceeds rates of other types of youth violence.” National Domestic Violence Hotline, 2014."
+}
+
+struct Q2 {
+    static let question = "What is the annual cost to society of domestic violence?"
+    static let a = "$300 million"
+    static let b = "$875 million"
+    static let c = "$1.2 billion"
+    static let d = "$5.8 billion"
+    static let answer = "d"
+    static let details = "Domestic violence costs include those associated with healthcare, law enforcement, judiciary, and social services.Costs associated with healthcare and services is estimated to be $4.1 billion per year."
+}
+
+struct Q3 {
+    static let question = "Which of the following is true about the effects of domestic violence on children?"
+    static let a = "Children exposed to domestic violence are more likely to be anxious than those not exposed."
+    static let b = "Children who witness domestic abuse have higher risks of alcohol/drug abuse, post traumatic stress syndrome, and juvenile delinquency."
+    static let c = "Approximately 3-4 million children between the ages of 3-17 are exposed to domestic violence each year."
+    static let d = "All of the above."
+    static let answer = "d"
+    static let details = "Research shows children (3-4 million per year), who are exposed to domestic violence tend to have more emotional and behavioral problems, such as anxiety, alcohol/drug use, post-traumatic stress syndrome and juvenile delinquency. Frequently, they replicate the violence they have witnessed during their teen years, in their adult relationships, and as parents."
+}
+
+struct Q4 {
+    static let question = "Which of the following could be signs of domestic violence?"
+    static let a = "Injuries on forearms, bruises, bite marks, rape"
+    static let b = "Physical threats, belittling behavior, controlling behavior"
+    static let c = "Withholding money or credit cards, restricting patient to an allowance, stealing from the patient"
+    static let d = "All of the above"
+    static let answer = "d"
+    static let details = "There might be physical, sexual, emotional, and/or financial abuse. Never assume that an injury is an accident, especially when accompanied with behavioral signs of violence. Routine assessment and screening for domestic violence is recommended."
+}
+
+struct Q5 {
+    static let question = "Screening: Which of these is an appropriate question to ask the patient during screening?"
+    static let a = "What did you do to make him hit you?"
+    static let b = "Do you feel you are in danger?"
+    static let c = "Have you tried marriage counseling?"
+    static let d = "Why did you wait so long to say something?"
+    static let answer = "b"
+    static let details = "Patient safety is the priority. Personal opinions and accusatory remarks can result in the patient feeling like they have done something wrong."
+}
+
+struct Q6 {
+    static let question = "Which of the following is NOT a supportive follow-up statement to a patient revealing evidence of domestic violence?"
+    static let a = "You are not alone."
+    static let b = "I’m sorry you have been hurt."
+    static let c = "If it were me, I wouldn’t put up with this."
+    static let d = "Thank you for telling me. I know it was difficult to do."
+    static let answer = "c"
+    static let details = "Show support and empathy when interacting with the patient, rather than accusing or judging the patient. This is useful in gaining the patient’s confidence and trust."
+}
+
+struct Q7 {
+    static let question = "Why should a patient that is a victim of domestic violence have a safety plan?"
+    static let a = "The patient may not have identified a support system."
+    static let b = "The patient might not think clearly during stressful situations due to adrenaline, so it’s good to plan ahead."
+    static let c = "The patient may not have time to gather the necessary items when escaping."
+    static let d = "All of the above"
+    static let answer = "d"
+    static let details = "All of the above answers help the patient/victim be better prepared for the next episode of violence by giving them a personalized and practical plan."
+}
+
+struct Q8 {
+    static let question = "Which of the following is NOT a way to increase the safety of a child in the home?"
+    static let a = "Teach them to call 911 and to know their address and phone number."
+    static let b = "Make a code word with them to call 911."
+    static let c = "Teach them how to break up a fight between their parents."
+    static let d = "Plan an escape route"
+    static let answer = "c"
+    static let details = "Children should be kept as far away from any acts of domestic violence as possible."
+}
+
+struct Q9 {
+    static let question = "Which of the following is FALSE about the effects of drugs and alcohol on domestic violence?"
+    static let a = "Perpetrators frequently use alcohol and drugs to rationalize their actions."
+    static let b = "Alcohol and drugs cause domestic violence."
+    static let c = "Drugs and alcohol do not cause the violence, but can lower the inhibitions of perpetrators."
+    static let answer = "c"
+    static let details = "As stated in parts A and C, while alcohol and drugs can alter the state of a abuser, these substances should not be blamed for their violent or abusive actions."
+}
+
+struct Q10 {
+    static let question = "Which of the following is a reason a patient might give for not leaving a relationship?"
+    static let a = "Belief that they are the only one who can help the abuser with their problems."
+    static let b = "Belief that the abusive partner will change because of his/her remorse and promises to stop battering."
+    static let c = "Lack of financial resources and anxiety about making a major life change."
+    static let d = "Concerns over breaking the family up and guilt associated with a failed relationship."
+    static let e = "All of the above"
+    static let answer = "e"
+    static let details = "There are many reasons why a patient might feel unable to leave. As a healthcare provider, you should refrain from passing judgement and focus on assisting them in creating safety plan and providing other helpful resources."
+}
+
+struct Q11 {
+    static let question = "Which of the following is NOT a correct practice for documenting domestic violence?"
+    static let a = "Use phrases such as the “patient claims” or “patient alleges.”"
+    static let b = "Set off the patient’s own words in quotation marks or use such phrases as “patient states” or “patient reports” to indicate that the information recorded reflects the patient’s words."
+    static let c = "Describe the patient’s demeanor, indicating, for example, whether she is crying or shaking or seems angry, agitated, upset, calm, or happy."
+    static let d = "Record the time of day the patient is examined and, if possible, indicate how much time has elapsed since the abuse occurred."
+    static let e = "Describe physical injuries, taking photos if consistent with facility policy."
+    static let answer = "a"
+    static let details = "Remember that all documentation is admissible in court and can be vital to a patient’s case. The above phrases imply doubt about the patient’s reliability. In general, be as detailed and factual as possible with documentation, using medical terminology as opposed to legal terms."
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ iOS/HeroesManual/ViewControllers/HealthcareQuizViewController.swift
@@ -0,0 +1,52 @@
+//
+//  HealthcareQuizViewController.swift
+//  HeroesManual
+//
+//  Created by Tucker on 3/9/18.
+//  Copyright © 2018 Team Simba. All rights reserved.
+//
+
+import UIKit
+
+class HealthcareQuizViewController: GeneralMenuController {
+    
+    // MARK: Properties
+    
+    // MARK: Actions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let navBar = self.navigationController?.navigationBar
+        navBar?.barTintColor = SIMBA_UI_COLORS.purple
+        navBar?.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
+        self.navigationItem.title = "Test Your Knowledge"
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if (self.isMovingFromParentViewController) {
+            
+            // Return navbar styling back to normal
+            let navBar = self.navigationController?.navigationBar
+            navBar?.prefersLargeTitles = false
+            navBar?.barTintColor = UIColor.white
+            navBar?.tintColor = SIMBA_UI_COLORS.green
+            navBar?.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
+            self.navigationItem.largeTitleDisplayMode = .never
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+
+
+
