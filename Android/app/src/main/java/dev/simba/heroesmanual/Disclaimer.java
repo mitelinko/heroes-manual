c11cf9f7bacccefae9e8c76e2bbe70154aa2f13c
Dimitar Pankov <mitelinko@gmail.com>
2018-04-23 03:04:33 -0400

    Added quiz to test knowledge on domestic violence.
    Added quiz to test knowledge on domestic violence. Added more icons. Updated button styles. Updated secondary color to match iOS. Added a feature graphic for Google Play Store. Added a 512x512 logo for Google Play Store. Bug fixes. Updated version to 1.1, version code 2.

--- Android/app/src/main/java/dev/simba/heroesmanual/Disclaimer.java
+++ Android/app/src/main/java/dev/simba/heroesmanual/Disclaimer.java
@@ -17,17 +17,17 @@ public class Disclaimer extends Activity {
         super.onCreate(savedInstanceState);
         getWindow().setStatusBarColor(getResources().getColor(R.color.primaryDarkColor));
         getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_disclaimer);
+
+        findViewById(R.id.buttonAcceptDisclaimer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), HealthcareHome.class));
+            }
+        });
 
         if (getSharedPreferences(getString(R.string.keyAppPreferences), Context.MODE_PRIVATE).getBoolean(getString(R.string.keyDisclaimerAccepted), false)) {
-            startActivity(new Intent(this, HealthcareHome.class));
-        } else {
-            setContentView(R.layout.activity_disclaimer);
-            findViewById(R.id.buttonAcceptDisclaimer).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    startActivity(new Intent(getApplicationContext(), HealthcareHome.class));
-                }
-            });
+            startActivity(new Intent(this, HealthcareHome.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
         }
     }
 }
